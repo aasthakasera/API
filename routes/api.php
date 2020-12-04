8d4dad57503b30f3145e68a9d2ad397776a06704
Aastha Kasera <aastha@Aasthas-MacBook-Air.local>
2020-12-04 15:46:04 +0530

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+//Route::apiResource('/products','ProductController'); 
+Route::apiResource('/products', 'App\Http\Controllers\ProductController');
+
+Route::group(['prefix'=>'products'],function(){
+
+    Route::apiResource('/{product}/reviews','App\Http\Controllers\ReviewController');
 });
